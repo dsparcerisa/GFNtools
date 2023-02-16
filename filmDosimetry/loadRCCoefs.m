880cb01d1284399f79bace486cb76946aae1b79f
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2023-02-16 15:34:44 +0100

    Script de carga automática de la calibracion

---
+++ filmDosimetry/loadRCCoefs.m
@@ -0,0 +1,8 @@
+%%% GFNTools %%%
+%%% (C) Daniel Sánchez Parcerisa 2023 %%%
+
+basePath = [fileparts(which('loadRCCoefs')) filesep 'calibrations']
+calFile = 'fitCoef_FQS_EBT3.mat';
+fullCalFilePath = [basePath filesep calFile];
+load(fullCalFilePath);
+fprintf('Loaded calibration file at %s\n', fullCalFilePath);
