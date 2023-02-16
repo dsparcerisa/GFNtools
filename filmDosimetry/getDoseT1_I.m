c9fa6110346e526980a2e43ac35ed649fa09f09c
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2023-02-16 15:55:34 +0100

    Nueva implementacion de coeficientes globales

--- filmDosimetry/getDoseT1_I.m
+++ filmDosimetry/getDoseT1_I.m
@@ -1,25 +1,27 @@
 %%% GFNTools %%%
 %%% (C) Daniel Sánchez Parcerisa 2023 %%%
 
-function [D, dD] = getDoseT1_I(I, coefs, pxmax, dI)
+function [D, dD] = getDoseT1_I(I, pxmax, dI)
+
+    global RCCalCoefs
+    
     % Validate inputs
     validateattributes(pxmax,'numeric',{'positive'});
     intClasses = {'uint8','uint16','uint32','uint64','double'};
     validateattributes(I,intClasses,{'nonnegative','3d','<=',pxmax})
-    validateattributes(coefs,'double',{'size',[3 3 2]});
-    if nargin>3
+    if nargin>2
         validateattributes(dI,'numeric',{'nonnegative','size',size(I)});
     else
         dI = 0*double(I);
     end
    
     % Define values
-    CoefR =  coefs(1,:,1);
-    CoefG =  coefs(2,:,1);
-    CoefB =  coefs(3,:,1);
-    dCoefR =  coefs(1,:,2);
-    dCoefG =  coefs(2,:,2);
-    dCoefB =  coefs(3,:,2);
+    CoefR =  RCCalCoefs(1,:,1);
+    CoefG =  RCCalCoefs(2,:,1);
+    CoefB =  RCCalCoefs(3,:,1);
+    dCoefR =  RCCalCoefs(1,:,2);
+    dCoefG =  RCCalCoefs(2,:,2);
+    dCoefB =  RCCalCoefs(3,:,2);
     R = double(I(:,:,1)) / pxmax;
     G = double(I(:,:,2)) / pxmax;
     B = double(I(:,:,3)) / pxmax;
--- filmDosimetry/loadRCCoefs.m
+++ filmDosimetry/loadRCCoefs.m
@@ -6,3 +6,12 @@ calFile = 'fitCoef_FQS_EBT3.mat';
 fullCalFilePath = [basePath filesep calFile];
 load(fullCalFilePath);
 fprintf('Loaded calibration file at %s\n', fullCalFilePath);
+
+global RCCalCoefs
+RCCalCoefs = zeros(3,3,2);
+RCCalCoefs(1,:,1) = CoefR1;
+RCCalCoefs(2,:,1) = CoefG1;
+RCCalCoefs(3,:,1) = CoefB1;
+RCCalCoefs(1,:,2) = dCoefR1;
+RCCalCoefs(2,:,2) = dCoefG1;
+RCCalCoefs(3,:,2) = dCoefB1;
