dc82277f01082484496ea0455104b5d7f096c5bf
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2023-05-17 12:47:03 +0200

    Nueva calibracion tras el reescaneo

--- filmDosimetry/loadRCCoefs.m
+++ filmDosimetry/loadRCCoefs.m
@@ -2,16 +2,24 @@
 %%% (C) Daniel Sánchez Parcerisa 2023 %%%
 
 basePath = [fileparts(which('loadRCCoefs')) filesep 'calibrations'];
-calFile = 'fitCoef_FQS_EBT3.mat';
+calFile = 'fitCoef_FQS_EBT3_rescan.mat';
 fullCalFilePath = [basePath filesep calFile];
 load(fullCalFilePath);
 fprintf('Loaded calibration file at %s\n', fullCalFilePath);
 
 global RCCalCoefs
 RCCalCoefs = zeros(3,3,2);
-RCCalCoefs(1,:,1) = CoefR1;
-RCCalCoefs(2,:,1) = CoefG1;
-RCCalCoefs(3,:,1) = CoefB1;
-RCCalCoefs(1,:,2) = dCoefR1;
-RCCalCoefs(2,:,2) = dCoefG1;
-RCCalCoefs(3,:,2) = dCoefB1;
+RCCalCoefs(1,:,1) = CoefR1_siHDR;
+RCCalCoefs(2,:,1) = CoefG1_siHDR;
+RCCalCoefs(3,:,1) = CoefB1_siHDR;
+RCCalCoefs(1,:,2) = dCoefR1_siHDR;
+RCCalCoefs(2,:,2) = dCoefG1_siHDR;
+RCCalCoefs(3,:,2) = dCoefB1_siHDR;
+
+% RCCalCoefs(1,:,1) = CoefR1;
+% RCCalCoefs(2,:,1) = CoefG1;
+% RCCalCoefs(3,:,1) = CoefB1;
+% RCCalCoefs(1,:,2) = dCoefR1;
+% RCCalCoefs(2,:,2) = dCoefG1;
+% RCCalCoefs(3,:,2) = dCoefB1;
+
