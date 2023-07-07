e02067b1d7cfdc22aa9a6c602026ac60f1f6181c
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2023-07-07 14:50:17 +0200

    Modificacion mínima RCset

--- filmDosimetry/examples/loadRCset/loacRCset_script.m
+++ filmDosimetry/examples/loadRCset/loacRCset_script.m
@@ -14,38 +14,6 @@ allstdIMgs = {allstdIMgs{:}, stdIMgs{:}};
 clearvars IMgs names RCfile RCpath stdIMgs
 
 %% Save all
-% numScans = [4
-% 12
-% 16
-% 21
-% 25
-% 29
-% 36
-% 43
-% 50
-% 57
-% 64
-% 71
-% 81
-% 85
-% 92
-% 99];
-times_min = [2
-12
-22
-52
-70
-98
-120
-182
-241
-298
-357
-450
-585
-734
-1446
-2825];
-numScans = 2*ones(size(times_min));
-saveName = 'Call.mat';
+numScans = 3;
+saveName = 'imageSet.mat';
 save(saveName);
